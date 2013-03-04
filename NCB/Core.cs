c1c5631d3a79b49338f84f2394d3f93d23387c15
Petr Bena <benapetr@gmail.com>
2013-03-04 17:27:24 +0100

    created

---
+++ NCB/Core.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Text;
+
+namespace NCB
+{
+    class Core
+    {
+        public static void WriteLog(string log)
+        { 
+            Console.WriteLine(DateTime.Now.ToString() + " SYSLOG: " + log);
+            return;
+        }
+
+        public static void handleException(Exception what)
+        {
+            WriteLog("Exception: " + what.ToString());
+        }
+
+        public static void LoadConf()
+        { 
+            
+        }
+
+        public static void Load()
+        { 
+            
+        }
+    }
+}
